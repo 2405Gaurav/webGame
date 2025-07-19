55278aa752b7fd3344643a5dbef661c6bd261148
Gaurav <gauravcoder3512@gmail.com>
2025-07-19 19:45:10 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Hero from "./components/Hero"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Hero/>
+    <About/>
+
     </>
   )
 }
